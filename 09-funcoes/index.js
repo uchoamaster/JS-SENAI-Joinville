132c1b9e1b4beb26670507888fa5ff3d2f18e4f2
professor <professor@SN-364401.edu.fiesc.com.br>
2023-04-27 18:23:22 -0300

    criando aulas novas de JS

---
+++ 09-funcoes/index.js
@@ -0,0 +1,208 @@
+// 1 - criando uma função
+function minhaFuncao() {
+    console.log("Testando!");
+  }
+  
+  minhaFuncao();
+  
+  const minhaFuncaoEmVariável = function () {
+    console.log("Função em variável!");
+  };
+  
+  minhaFuncaoEmVariável();
+  
+  function funcaoComParametro(txt) {
+    console.log(`Imprimindo: ${txt}`);
+  }
+  
+  funcaoComParametro("Opa");
+  
+  // 2 - return
+  const a = 10;
+  const b = 20;
+  const c = 30;
+  const d = 40;
+  
+  function soma(n1, n2) {
+    return n1 + n2;
+  }
+  
+  const resultado = soma(a, b);
+  
+  console.log(resultado);
+  
+  console.log(soma(c, d));
+  
+  // 3 - escopo da função
+  let y = 10;
+  
+  function testandoEscopo() {
+    let y = 20;
+    console.log(`Y dentro da função: ${y}`);
+  }
+  
+  console.log(`Y fora da função: ${y}`);
+  
+  testandoEscopo();
+  
+  // 4 - escopo aninhado
+  let m = 10;
+  
+  function escopoAninhado() {
+    let m = 20;
+  
+    if (true) {
+      let m = 30;
+  
+      console.log(m);
+    }
+  
+    console.log(m);
+  }
+  
+  escopoAninhado();
+  
+  console.log(m);
+  
+  // 5 - arrow function
+  const testeArrow = () => {
+    console.log("Esta é uma arrow function");
+  };
+  
+  testeArrow();
+  
+  const parOuImpar = (n) => {
+    if (n % 2 === 0) {
+      console.log("Par");
+      return;
+    }
+  
+    console.log("Impar");
+  };
+  
+  parOuImpar(5);
+  parOuImpar(6);
+  
+  // 6 - mais sobre arrow functions
+  const raizQuadrada = (x) => {
+    return x * x;
+  };
+  
+  const raizQuadrada2 = (n) => n * n;
+  
+  console.log(raizQuadrada(4));
+  console.log(raizQuadrada2(4));
+  
+  const helloWorld = () => console.log("Hello!");
+  
+  helloWorld();
+  
+  // 7 - parametro opcional
+  const multiplication = function (n, m) {
+    if (m === undefined) {
+      return n * 2;
+    } else {
+      return m * n;
+    }
+  };
+  
+  console.log(multiplication(4, 4));
+  console.log(multiplication(4));
+  
+  const greeting = (name) => {
+    if (!name) {
+      console.log("Olá!");
+      return;
+    }
+  
+    console.log(`Olá ${name}!`);
+  };
+  
+  greeting();
+  greeting("Carlos");
+  
+  // 8 - valor default
+  const customGreeting = (name, greet = "Olá") => {
+    return `${greet}, ${name}!`;
+  };
+  
+  console.log(customGreeting("Carlos"));
+  
+  console.log(customGreeting("João", "Bom dia"));
+  
+  const repeatText = (text, repeat = 2) => {
+    for (let i = 0; i < repeat; i++) {
+      console.log(text);
+    }
+  };
+  
+  repeatText("Testando");
+  repeatText("Agora repete 5 vezes", 5);
+  
+  // 9 - closure
+  function someFunction() {
+    let txt = "Alguma coisa";
+  
+    function display() {
+      console.log(txt);
+    }
+  
+    display();
+  }
+  
+  someFunction();
+  
+  // 10 - mais sobre closure
+  const multiplicationClosure = (n) => {
+    return (m) => {
+      return n * m;
+    };
+  };
+  
+  const c1 = multiplicationClosure(5);
+  
+  const c2 = multiplicationClosure(10);
+  
+  console.log(c1);
+  console.log(c2);
+  
+  console.log(c1(5));
+  console.log(c2(10));
+  
+  // 11 - recursion
+  const untilTen = (n, m) => {
+    if (n < 10) {
+      console.log("A função parou de executar!");
+    } else {
+      const x = n - m;
+  
+      console.log(x);
+  
+      untilTen(x, m);
+    }
+  };
+  
+  untilTen(100, 7);
+  
+  // infinite recursion, tomar cuidado
+  // function run() {
+  //   console.log("Executando...");
+  //   run();
+  // }
+  
+  // run();
+  
+  function factorial(x) {
+    if (x === 0) {
+      return 1;
+    } else {
+      return x * factorial(x - 1);
+    }
+  }
+  
+  const num = 6;
+  
+  const result = factorial(num);
+  
+  console.log(`O fatorial do número ${num} é ${result}`);
+  
---
+++ 11-eventos/js/scripts.js
@@ -0,0 +1,134 @@
+// 1 - adicionando eventos
+const btn = document.querySelector("#my-button");
+
+btn.addEventListener("click", function () {
+  console.log("Clicou aqui!");
+});
+
+// 2 - removendo evento
+const secondBtn = document.querySelector("#btn");
+
+function imprimirMensagem() {
+  console.log("Teste");
+}
+
+secondBtn.addEventListener("click", imprimirMensagem);
+
+const thirdBtn = document.querySelector("#other-btn");
+
+thirdBtn.addEventListener("click", () => {
+  console.log("Evento removido");
+  secondBtn.removeEventListener("click", imprimirMensagem);
+});
+
+// 3 - argumento de evento
+const title = document.querySelector("#my-title");
+
+title.addEventListener("click", (e) => {
+  console.log(e);
+  console.log(e.srcElement);
+  console.log(e.offsetX);
+  console.log(e.pointerType);
+});
+
+// 4 - propagação
+const containerBtn = document.querySelector("#btn-container");
+const btnInsideContainer = document.querySelector("#div-btn");
+
+containerBtn.addEventListener("click", () => {
+  console.log("Evento 1");
+});
+
+btnInsideContainer.addEventListener("click", (e) => {
+  // sem isso acontece a propagação
+  e.stopPropagation();
+  console.log("Evento 2");
+});
+
+// 5 - removendo efeito padrão
+const a = document.querySelector("a");
+
+a.addEventListener("click", (e) => {
+  e.preventDefault();//previne ação 
+  console.log("Não alterou a página");
+});
+
+// 6 - eventos de tecla
+document.addEventListener("keyup", (e) => {
+  console.log(`Soltou a tecla ${e.key}`);
+});
+
+document.addEventListener("keydown", (e) => {
+  console.log(`Pressionou a tecla ${e.key}`);
+});
+
+// 7 - outros eventos de mouse
+const mouseEvents = document.querySelector("#mouse");
+
+mouseEvents.addEventListener("mousedown", () => {
+  console.log("Pressionou botão");
+});
+
+mouseEvents.addEventListener("mousedown", () => {
+  console.log("Soltou botão");
+});
+
+mouseEvents.addEventListener("dblclick", () => {
+  console.log("Clique duplo");
+});
+
+// 8 - movimento do mouse
+document.addEventListener("mousemove", (e) => {
+  // console.log(`No eixo X: ${e.x}`);
+  // console.log(`No eixo Y: ${e.y}`);
+});
+
+// 9 - evento no scroll
+window.addEventListener("scroll", (e) => {
+  if (window.pageYOffset > 200) {
+    console.log("Passamos 200px!");
+  }
+});
+
+// 10 - evento de focus
+const input = document.querySelector("#my-input");
+
+input.addEventListener("focus", (e) => {
+  console.log("Entrou no input!");
+});
+
+input.addEventListener("blur", (e) => {
+  console.log("Saiu do input!");
+});
+
+// 11 - evento de carregamento
+window.addEventListener("load", () => {
+  console.log("Página carregou!");
+});
+
+window.addEventListener("beforeunload", (event) => {
+  event.preventDefault();
+});
+
+// 12 - debounce
+
+const debounce = (f, delay) => {
+  let timeout;
+
+  return (...arguments) => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(() => {
+      f.apply(arguments);
+    }, delay);
+  };
+};
+
+window.addEventListener(
+  "mousemove",
+  debounce(() => {
+    console.log("Executando a cada 400ms");
+  }, 400)
+);
